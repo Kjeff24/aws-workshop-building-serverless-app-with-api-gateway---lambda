e0283d0e0e1fc7d789083951b5324288ab043db3
kjeff24 <jeffreyarthur123@gmail.com>
2024-11-11 18:14:40 +0000

    Complete module-3 2

--- users/tests/integration/test_api.py
+++ users/tests/integration/test_api.py
@@ -7,40 +7,32 @@ import requests
 new_user_id = ""
 new_user = {"name": "John Doe"}
 
-
 def test_access_to_the_users_without_authentication(global_config):
     response = requests.get(global_config["APIEndpoint"] + '/users')
     assert response.status_code == 401
 
-
 def test_get_list_of_users_by_regular_user(global_config):
     response = requests.get(
         global_config["APIEndpoint"] + '/users',
-        headers={'Authorization': global_config["regularUserIdToken"]},
+        headers={'Authorization': global_config["regularUserIdToken"]}
     )
     assert response.status_code == 403
 
-
-def test_deny_put_user_by_regular_user(global_config):
-    response = requests.put(
+def test_deny_post_user_by_regular_user(global_config):
+    response = requests.post(
         global_config["APIEndpoint"] + '/users',
         data=json.dumps(new_user),
-        headers={
-            'Authorization': global_config["regularUserIdToken"],
-            'Content-Type': 'application/json',
-        },
+        headers={'Authorization': global_config["regularUserIdToken"],
+                 'Content-Type': 'application/json'}
     )
     assert response.status_code == 403
 
-
-def test_allow_put_user_by_administrative_user(global_config):
-    response = requests.put(
+def test_allow_post_user_by_administrative_user(global_config):
+    response = requests.post(
         global_config["APIEndpoint"] + '/users',
         data=json.dumps(new_user),
-        headers={
-            'Authorization': global_config["adminUserIdToken"],
-            'Content-Type': 'application/json',
-        },
+        headers={'Authorization': global_config["adminUserIdToken"],
+                 'Content-Type': 'application/json'}
     )
     assert response.status_code == 200
     data = json.loads(response.text)
@@ -48,23 +40,19 @@ def test_allow_put_user_by_administrative_user(global_config):
     global new_user_id
     new_user_id = data['userid']
 
-
-def test_deny_put_invalid_user(global_config):
+def test_deny_post_invalid_user(global_config):
     new_invalid_user = {"Name": "John Doe"}
-    response = requests.put(
+    response = requests.post(
         global_config["APIEndpoint"] + '/users',
         data=new_invalid_user,
-        headers={
-            'Authorization': global_config["adminUserIdToken"],
-            'Content-Type': 'application/json',
-        },
+        headers={'Authorization': global_config["adminUserIdToken"],
+                 'Content-Type': 'application/json'}
     )
     assert response.status_code == 400
-
 
 def test_get_user_by_regular_user(global_config):
     response = requests.get(
         global_config["APIEndpoint"] + f'/users/{new_user_id}',
-        headers={'Authorization': global_config["regularUserIdToken"]},
+        headers={'Authorization': global_config["regularUserIdToken"]}
     )
     assert response.status_code == 403
